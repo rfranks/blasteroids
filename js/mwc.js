94512325ef0974740cf23d8be557aa605c69b7af
rfrankspk <rfranks@patientkeeper.com>
2022-01-17 13:37:24 -0500

    Trying to fix CodeQL scanning alerts.

--- js/mwc.js
+++ js/mwc.js
@@ -15,9 +15,9 @@ for (const tabBar of tabBars) {
 
 $('.mdc-tab-bar').on('click', '[data-tab-contents]', function (e) {
     var $this = $(this),
-          $tabContents = $($this.attr('data-tab-contents')),
-          src = $tabContents && $tabContents.length && $tabContents.attr('data-tab-src'),
-          redirect = $this.attr('data-redirect');
+        $tabContents = $($.find($this.attr('data-tab-contents'))[0]),
+        src = $tabContents && $tabContents.length && $tabContents.attr('data-tab-src'),
+        redirect = $this.attr('data-redirect');
 
     if (redirect && redirect.length) {
         window.location.href = redirect;
@@ -32,7 +32,7 @@ $('.mdc-tab-bar').on('click', '[data-tab-contents]', function (e) {
         src &&
             src.length &&
             !$tabContents.find('iframe').length &&
-            $tabContents.append('<iframe src="' + src + '"></iframe>');
+            $tabContents.append($('<iframe></iframe>').attr('src', src));
     }
 });
 
